08d841d1886ea85eb20a0c2b1f707fff52e77da2
Cultis <samu.teitti@gmail.com>
2016-11-03 17:17:52 +0200

    IoT umbrella rack

--- Umbrella_rack.ino
+++ Umbrella_rack.ino
@@ -1,22 +1,43 @@
+#include <ESP8266WiFi.h>
+
 // Wifi access point
-#define SSID        "XXXX"                          
-#define PASS        "XXXX"                           
+#define WLAN_SSID   "XXXX"                          
+#define WLAN_PASS   "XXXX"                           
 
 // Weather Underground setup
 #define DEST_HOST   "api.wunderground"             
-#define DEST_IP     "192.254.235.21"                 
+#define DEST_IP     "192.254.235.21"      
 
 void setup() 
 {
+// Set datarate
+  Serial.begin(115200);
+  delay(10);
 
-// Initialize the BUILTIN_LED pin as an output 
+// Define LED pins as an output 
   pinMode(16, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(4, OUTPUT); 
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
 }
 
-void loop() {
-
+void loop() 
+{
+  
 // LED triggers based on API data
   if (APIrain => 10 && APIrain <= 30)  
   {
